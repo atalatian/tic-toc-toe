8e61514541ae50033476b909e1d2111a77436bfa
Amir Hossein Talatian <53391826+atalatian@users.noreply.github.com>
2020-09-26 12:33:19 +0330

    Update asgi.py

--- game/asgi.py
+++ game/asgi.py
@@ -10,7 +10,9 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-
+from channels.routing import get_default_application
+import django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'game.settings')
+django.setup()
 
-application = get_asgi_application()
+application = get_default_application()
